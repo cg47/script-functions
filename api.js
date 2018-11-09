6eea5f1d8ff1eb66fbeb295b41db274eaf735e14
Amélie Lacroix <churchanopolis@gmail.com>
2018-11-09 03:12:47 -0700

    JavaScript

--- api.js
+++ api.js
@@ -1,5 +1,7 @@
 function flip_boolean( bool ) { // ( null || false ) ? true : false
- if( bool === true ) return( false );
+ if( bool === true ) {
+  return( false );
+ }
  return( true );
 }
 function is_empty_or_null( object ) { // ( null || invalid object || length == 0 ) ? true : false
@@ -31,7 +33,7 @@ function elem_classSwap( elem, class_from, class_to ) { // elem_classSwap( docum
  var classes = ( " " + elem_class + " " ).replace( " " + class_from + " ", " " + class_to + " " );
  elem.setAttribute( "class", classes.substr( 1, classes.length - 2 ) );
 }
-function array_sub( array, start, length ) { // array_sub( [ 0x0D, 0x0A, 0xDE, 0xAD, 0xBE, 0xEF ], 2, 2 ) = [ 0xDE, 0xAD ]
+function array_sub( array, start, length ) { // array_sub( [ 0x0D, 0x0A, 0xDE, 0xAD, 0xBE, 0xEF ], 0x2, 2 ) = [ 0xDE, 0xAD ]
  return( array.slice( start, start + length ) );
 }
 function array_toggle( array, toggle, method ) { // array_toggle( [ "BEEF" ], "DEAD", "unshift" ) = [ "DEAD", "BEEF" ]
@@ -54,17 +56,26 @@ function byte2dataBlockLength( byte ) { // byte2dataBlockLength( 0x7F ) = 0x3F8
  return( byte * 8 );
 }
 function bytes2hex( bytes ) { // bytes2hex( [ 0x0D, 0x0A ] ) = "0D0A"
+ bytes = ( typeof( bytes ) !== "undefined" && bytes instanceof Array ) ? bytes : [ bytes ];
  return( Array.from( bytes, function( byte ) {
   return( ( "0" + ( byte & 0xFF ).toString( 16 ) ).slice( -2 ) );
  } ).join( "" ).toUpperCase() );
 }
 function bytes2int( bytes ) { // bytes2int( [ 0xDE, 0xAD ] ) = 0xDEAD
- return( bytes[ 1 ] | bytes[ 0 ] << 8 );
+ bytes = ( typeof( bytes ) !== "undefined" && bytes instanceof Array ) ? bytes : [ bytes ];
+ var int = 0;
+ var mod = 0;
+ for( var i = 0; i < bytes.length; i++ ) {
+  int |= bytes[ bytes.length - ( i + 1 ) ] << mod;
+  mod += 8;
+ }
+ return( int );
 }
 function bytes2string( bytes ) { // bytes2string( [ 0x0D, 0x0A ] ) = "\r\n"
+ bytes = ( typeof( bytes ) !== "undefined" && bytes instanceof Array ) ? bytes : [ bytes ];
  var string = "";
  for( var i = 0; i < bytes.length; i++ ) {
-  string += String.fromCharCode( parseInt( bytes[ i ], 2 ) );
+  string += String.fromCharCode( parseInt( bytes[ i ] ) );
  }
  return( string );
 }
@@ -82,4 +93,27 @@ function hex2string( hex ) { // hex2string( "0D0A" ) = "\r\n"
   string += String.fromCharCode( parseInt( hex.substr( i, 2 ), 16 ) );
  }
  return( string );
+}
+function dataBlocks2bytes( start, bytes, stop_at_byte ) { // dataBlocks2bytes( 0, [ 0x02, 0xDE, 0xAD, 0xFF, 0x02, 0xBE, 0xEF ], 0xFF ) = [ [ 0xDE, 0xAD ], 0x03 ]
+ start = start || 0;
+ stop_at_byte = stop_at_byte || -1;
+ var all_blocks_end_at_index = start;
+ var a = new Array();
+ for(;;) {
+  var block_length = bytes[ start ] * 8;
+  for( var i = 1; i <= block_length; i++ ) {
+   var b = bytes[ start + i ];
+   if( b == stop_at_byte ) {
+    all_blocks_end_at_index = start + i;
+    break;
+   }
+   a.push( b );
+  }
+  start += block_length + 1;
+  all_blocks_end_at_index = start - 1;
+  if( block_length != 0x7F * 8 ) {
+   break;
+  }
+ }
+ return( [ a, all_blocks_end_at_index ] );
 }
